b3ae9ab905a15a11423ed2c9fda1c2e11c8f03c8
emb-develop <62819123+emb-develop@users.noreply.github.com>
2021-08-21 17:18:22 -0400

    Add files via upload

---
+++ js/active.js
@@ -0,0 +1,35 @@
+addEventListener("DOMContentLoaded", () => {
+  let list_nav = document.querySelector(".list_nav");
+  let items = document.querySelectorAll(".list_nav li a");
+
+  items.forEach((data) => {
+    data.addEventListener("click", function () {
+      list_nav.querySelector(".active").classList.remove("active");
+      data.classList.add("active");
+    });
+  });
+
+  const sections = document.querySelectorAll(".scroll");
+  const funcion_observer = (entradas) => {
+    entradas.forEach((entrada) => {
+      if (entrada.isIntersecting) {
+        const entrada_actual = Array.from(items).find(
+          (item) => item.dataset.url === entrada.target.id
+        );
+        entrada_actual.classList.add("active");
+
+        for (const item of items) {
+          if (item != entrada_actual) {
+            item.classList.remove("active");
+          }
+        }
+      }
+    });
+  };
+  const observer = new IntersectionObserver(funcion_observer, {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.6,
+  });
+  sections.forEach((seccion) => observer.observe(seccion));
+});
---
+++ js/animations.js
@@ -0,0 +1,79 @@
+addEventListener("DOMContentLoaded", () => {
+  ScrollReveal().reveal(".container_head", {
+    duration: 2000,
+    reset: true,
+  });
+  ScrollReveal().reveal(".container__image", {
+    origin: "left",
+    distance: "50%",
+    duration: 3000,
+    reset: true,
+  });
+
+  ScrollReveal().reveal(".container__text > h2", {
+    origin: "top",
+    distance: "150%",
+    duration: 1000,
+    reset: true,
+  });
+  ScrollReveal().reveal(".container__text > h1", {
+    duration: 3000,
+    delay: 500,
+    reset: true,
+  });
+  ScrollReveal().reveal(".container__text > h3, article", {
+    duration: 3000,
+    delay: 1000,
+    reset: true,
+  });
+});
+
+ScrollReveal().reveal(".container__title", {
+  duration: 2000,
+  rotate: {
+    y: 180,
+  },
+  reset: true,
+});
+ScrollReveal().reveal(
+  ".container_nosotros__animation, .animation__services__one",
+  {
+    origin: "left",
+    distance: "50%",
+    duration: 1500,
+    reset: true,
+  }
+);
+ScrollReveal().reveal(".container_nosotros__mis-vis", {
+  delay: 1000,
+  duration: 1000,
+  reset: true,
+});
+ScrollReveal().reveal(".container_portafolio__secondary", {
+  origin: "top",
+  distance: "50%",
+  duration: 1000,
+  reset: true,
+});
+
+ScrollReveal().reveal(".container_nosotros_img, .animation__services__two", {
+  origin: "right",
+  distance: "50%",
+  duration: 1500,
+  reset: true,
+});
+ScrollReveal().reveal(".container_contacto_img", {
+  duration: 1000,
+  reset: true,
+  origin: "left",
+  distance: "50%",
+});
+ScrollReveal().reveal(".container__form", {
+  duration: 2000,
+  delay: 2000,
+  rotate: {
+    x: 180,
+    y: 180,
+  },
+  reset: true,
+});
